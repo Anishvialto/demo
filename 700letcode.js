dcd19965794661a70b78d7667fff08c6ae41ae85
Anishvialto <anish.kumar.kodathala@vialto.com>
2025-05-13 14:50:47 +0530

    string pr

---
+++ 700letcode.js
@@ -0,0 +1,34 @@
+class LongestwordD
+{
+    constructor(words)
+    {
+        this.words=words;
+    }
+
+    findlongestword()
+    {
+        this.words.sort();
+
+        let vaildwords={"":true};
+        let longest="";
+
+        for(let i=0;i<this.words.length;i++)
+        {
+            let word=this.words[i];
+            let perfix=word.substring(0,word.length-1);
+
+            if(vaildwords[perfix])
+            {
+                vaildwords[word]=true;
+
+                if(word.length>longest.length)
+                {
+                    longest=word;
+                }
+            }
+        }
+        return longest;
+    }
+}
+
+let 
